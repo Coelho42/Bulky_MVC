9b78ca08f5c936e319463698c05fa706c6b9a7c1
Goncalo Coelho <goncalocoelho@orbcom.pt>
2024-03-05 18:26:13 +0000

    Learned - Models, Views and Controllers - Data annotations - Created databases with tables - Migrations - Bootstrap - Tag Helpers - Received and added data to the database

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj); // Keeps track of what needs to be added
+            _db.SaveChanges(); // Saves the obj into the database
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
